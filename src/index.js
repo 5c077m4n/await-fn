10c004d6ea82fe9555ce9e52a5105a8ee9355394
Roee <roy.shap1@gmail.com>
2018-08-14 03:20:15 +0300

    Updated tests.

--- src/index.js
+++ src/index.js
@@ -3,27 +3,24 @@ const hr = require('http-responder');
 
 module.exports = (fn, options = {}) => {
 	const promise = bluebird.resolve(fn(...((options.params)? options.params : [undefined])))
+		.then(data => [undefined, data]);
 
 	if(!(options.throw && options.web)) {
 		return promise
-			.then(data => [undefined, data])
 			.catch(error => [error, undefined]);
 	}
 	if(options.throw && options.web) {
 		return promise
-			.then(data => [undefined, data])
 			.catch(error => {
 				throw hr.improve(error);
 			});
 	}
 	if(options.web) {
 		return promise
-			.then(data => [undefined, data])
 			.catch(error => [hr.improve(error), undefined]);
 	}
 	if(options.throw) {
 		return promise
-			.then(data => [undefined, data])
 			.catch(error => {
 				throw error;
 			});
--- test/index.js
+++ test/index.js
@@ -5,15 +5,21 @@ const should = require('chai').should();
 const to = require('../src/index');
 
 
+const wait = time => new Promise(resolve => setTimeout(resolve, time));
+
 describe('Test to function', function() {
 	describe('the waiting', function() {
 		it('should return 7 - no params', async function() {
 			let [error, data] = await to(() => 7);
 			expect(data).equal(7);
 		});
-		it('should return 7 - with params', async function() {
-			let [error, data] = await to(x => x++, { params: [5] });
+		it('should return 6 - with 1 param', async function() {
+			let [error, data] = await to(x => ++x, { params: [5] });
 			expect(data).equal(6);
+		});
+		it('should return 4 - with 2 params', async function() {
+			let [err, data] = await to((a, b) => a + b, { params: [1, 3] });
+			expect(data).equal(4);
 		});
 	});
 });
