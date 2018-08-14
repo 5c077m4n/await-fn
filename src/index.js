bd78243255f1e6ffea33f0822d9d62376535a0dd
Roee <roy.shap1@gmail.com>
2018-08-14 12:37:47 +0300

    Fixed testing.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ const Bluebird = require('bluebird');
 const hr = require('http-responder');
 
 module.exports = (fn, options = {}) => {
-	return Promise.resolve(fn(...((options.params)? options.params : [undefined])))
+	return new Bluebird((resolve, reject) => {
+		resolve(fn(...((options.params)? options.params : [undefined])));
+	})
 		.then(data => [undefined, data])
 		.catch(error => {
-			if(options.throw && options.web) throw hr.improve(error);
-			if(options.throw) throw error;
+			if(options.throw) Bluebird.reject(error);
 			if(options.web) return [hr.improve(error), undefined];
 			return [error, undefined];
+		})
+		.catch(error => {
+			if(options.web) throw [hr.improve(error), undefined];
+			throw [error, undefined];
 		});
 };
--- test/index.js
+++ test/index.js
@@ -6,9 +6,10 @@ const hr = require('http-responder');
 const to = require('../src/index');
 
 
+const log = console.log.bind(console);
 const factorial = x => (x > 0)? (x * factorial(x - 1)) : 1;
-const longJob = () => {
-	for(let i = 0; i < 1e7; i++) factorial(15);
+const longTask = () => {
+	for(let i = 0; i < 1e7; i++) factorial(10);
 	return 1;
 };
 
@@ -28,26 +29,34 @@ describe('Test to function', function() {
 		});
 	});
 	describe('the error handling', function() {
-		it('should throw the error', async function() {
+		it('should catch the error', async function() {
+			const [error, data] = await to(() => factorial(1e16));
+			expect(error instanceof Error).equal(true);
+		});
+		it('should catch the HTTP Responder object', async function() {
+			const [error, data] = await to(() => factorial(1e16), { web: true });
+			expect(hr.isHR(error)).equal(true);
+		});
+		it('should throw an error', async function() {
 			try {
-				await to(JSON.parse, { params: [123], throw: true });
+				await to(() => factorial(1e16), { throw: true });
 			}
 			catch(error) {
 				expect(error instanceof Error).equal(true);
 			}
 		});
-		it('should throw an http-responder error', async function() {
+		it('should throw an HTTP Responder object', async function() {
 			try {
-				await to(JSON.parse, { params: [123], throw: true, web: true });
+				await to(() => factorial(1e16), { throw: true, web: true });
 			}
 			catch(error) {
-				expect(error instanceof hr).equal(true);
+				expect(hr.isHR(error)).equal(true);
 			}
 		});
 	});
 	describe('the waiting', function() {
 		it('wait for the result', async function() {
-			let [error, data] = await to(longJob);
+			const [error, data] = await to(longTask);
 			expect(data).equal(1);
 		});
 	});
