ac7828a24fb8c16f4915fde6dd52d1a06ed4ae5e
Roee <roy.shap1@gmail.com>
2018-10-03 12:36:40 +0300

    Improved preformance.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,30 @@
 const Bluebird = require('bluebird');
 const hr = require('http-responder');
 
+
+const instanceOfArrayItems = arr => {
+	for(const el of arr)
+		if(el || (arr[0].constructor !== el.constructor)) return false;
+	return arr[0].constructor;
+};
+
 module.exports = (fn, options = {}) => {
 	let promise;
-	if(fn instanceof Function) {
+	if(fn.constructor === Function) {
 		promise = new Bluebird(resolve =>
-			resolve((options.params)? fn(...options.params) : fn(options.param))
+			resolve((options.params)?
+				fn(...options.params) : fn(options.param))
 		);
 	}
-	if(fn instanceof Promise || fn instanceof Bluebird) promise = fn;
+	if(fn.constructor === Promise || fn.constructor === Bluebird) promise = fn;
+	if(fn.constructor === Array) {
+		const arrType = instanceOfArrayItems(fn);
+		if(!arrType) return;
+		if(arrType === Function) {
+			fn = fn.map(async func => await Bluebird.promisify(func));
+		}
+		else promise = Bluebird.all(fn);
+	}
 
 	return promise
 		.then(data => (options.returnOne)? data : [undefined, data])
