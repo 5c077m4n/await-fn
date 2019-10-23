c726f464723bf25722cffea0d79faf802842613d
Roee Shapira <roy.shap1@gmail.com>
2019-10-23 23:40:51 +0300

    Added external eslint file.

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        node: true,
+        browser: true,
+        es6: true
+    },
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module"
+    },
+    extends: "eslint:recommended",
+    rules: {}
+};
