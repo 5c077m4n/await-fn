8d380921cfafa5bf2786cbdf5e0013f37e224cf0
Roee Shapira <roy.shap1@gmail.com>
2019-10-24 00:24:22 +0300

    Ugly workaround for require('http-responder').

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Bluebird = require('bluebird');
-const hr = require('http-responder');
+const hr = require('../node_modules/http-responder/src');
 
 
 const to = (fn, { params, param, returnOne, web, throwError } = {}) => {
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 const should = require('chai').should();
-const hr = require('http-responder');
+const hr = require('../node_modules/http-responder/src');
 
 const to = require('../src/index');
 // const to = require('../dist/to.bundle');
