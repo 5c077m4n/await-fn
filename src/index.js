5c9d3d143a36c3de1687d4f94c4c78ba6a32aba5
Roee Shapira <roy.shap1@gmail.com>
2019-10-26 03:01:42 +0300

    Improved build and code fixes.

--- src/index.js
+++ src/index.js
@@ -3,38 +3,40 @@
 const Bluebird = require('bluebird');
 const hr = require('http-responder');
 
-
 const to = (fn, { params, param, returnOne, web, throwError } = {}) => {
 	let promise;
 	if (fn.constructor === Function) {
-		promise = new Bluebird(resolve => {
-			return resolve(params ?
-				fn(...params) : fn(param)
-			)
-		});
-	}
-	if (fn.constructor === Promise || fn.constructor === Bluebird) promise = fn;
-	if (fn.constructor === Array) {
+		promise = new Bluebird(resolve =>
+			resolve(params ? fn(...params) : fn(param))
+		);
+	} else if (fn.constructor === Promise || fn.constructor === Bluebird) {
+		promise = fn;
+	} else if (Array.isArray(fn)) {
 		const promArr = fn.map(fnOrProm => {
 			if (fnOrProm.constructor === Function) {
 				return new Bluebird(resolve => {
-					return resolve(params ?
-						fnOrProm(...params) : fnOrProm(param)
-					)
+					return resolve(
+						params ? fnOrProm(...params) : fnOrProm(param)
+					);
 				});
 			}
-			if (fnOrProm.constructor === Promise || fnOrProm.constructor === Bluebird)
+			if (
+				fnOrProm.constructor === Promise ||
+				fnOrProm.constructor === Bluebird
+			) {
 				return fnOrProm;
-		})
+			}
+		});
 		promise = Bluebird.all(promArr);
+	} else {
+		throw new Error('There was an erron in your input function.');
 	}
 
 	return promise
-		.then(data => (returnOne) ? data : [undefined, data])
+		.then(data => (returnOne ? data : [undefined, data]))
 		.catch(error => {
 			if (throwError) return Bluebird.reject(error);
-			if (returnOne)
-				return web ? hr.improve(error) : error;
+			if (returnOne) return web ? hr.improve(error) : error;
 			return web ? [hr.improve(error), undefined] : [error, undefined];
 		})
 		.catch(error => {
@@ -47,5 +49,5 @@ else if (window || self) {
 	const global = window || self;
 	const originalTo = global.to;
 	global.to = to;
-	global.restoreOriginalTo = () => global.to = originalTo;
+	global.restoreOriginalTo = () => (global.to = originalTo);
 }
