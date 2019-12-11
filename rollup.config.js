373474cea069a08b8c315ba5a2cff852c16c5d70
Roee Shapira <roy.shap1@gmail.com>
2019-12-11 23:34:09 +0200

    Improved rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,17 @@
 import resolve from '@rollup/plugin-node-resolve';
 
-export default [
-	{
-		input: 'src/index.js',
-		output: {
+export default {
+	input: 'src/index.js',
+	output: [
+		{
 			dir: 'packages/await-fn.cjs/src/',
 			format: 'cjs',
 		},
-		plugins: [resolve()],
-	},
-	{
-		input: 'src/index.js',
-		output: {
+		{
 			name: 'to',
 			dir: 'packages/await-fn.umd/src/',
 			format: 'umd',
 		},
-		plugins: [resolve()],
-	},
-];
+	],
+	plugins: [resolve()],
+};
